9dc145d37f80f7170d8012f6b1031c3839b03ba6
Ruslan Galimov <galimov.ruslan.z@gmail.com>
2017-06-15 12:25:03 +0300

    123

--- mongo.py
+++ mongo.py
@@ -5,7 +5,26 @@ from pymongo import MongoClient
 # docker run -p 27017:27017 --name mongo1 mongo
 client = MongoClient('localhost', 27017)
 db = client.test_database
-posts = db.posts
+pcs = db['PC']
+confs = db['Confs']
+adapters = db['Adapters']
+
+pcs.drop()
+pc1 = pcs.insert_one({'name': 'ПБД', 'year': 1}).inserted_id
+pc2 = pcs.insert_one({'name': 'СПАСОИУ', 'year': 1, }).inserted_id
+
+confs.drop()
+confs.insert([{'name': '7', 'year': 1, 'department': '5', 'active': True},
+              {'name': '8', 'year': 1, 'department': '5', 'active': True}])
+
+adapters.drop()
+adapters.insert([{'pc': pc1}, {'pc': pc2}])
+for collection in [pcs, adapters, confs]:
+    print(collection.name)
+    for r in collection.find():
+        print(r)
+
+confs = db.confs
 post = {"pc": "1",
         "configuration": {
             'description': "My!",
@@ -14,8 +33,18 @@ post = {"pc": "1",
         "tags": ["mongodb", "python", "pymongo"],
         "date": datetime.datetime.utcnow()
         }
-post = posts.insert_one(post)
-print(post.inserted_id)
-print(posts.find_one({'_id': post.inserted_id}))
-for item in posts.find():
+post2 = {"pc": "2",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()
+        }
+
+post = confs.insert_one(post)
+confs.find_one({'_id': post.inserted_id})
+confs.find_one_and_replace({'_id': post.inserted_id}, post2)
+confs.update({'pc': 2}, {'$push': {'configuration': {'allow_internet': False}}})
+confs.update({'pc': 2}, {'$set': {"date": datetime.datetime.utcnow()}})
+confs.update({'pc': 2}, {'$unset': {"tags": ''}})
+confs.remove({'pc': 2})
+
+for item in confs.find():
     print(item)
